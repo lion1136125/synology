ca370819c859cdf615d962c12b418c194520e68b
lion1136125 <lion1496427@gmail.com>
2025-11-18 01:41:52 +0900

    Delete script-v2.js

--- script-v2.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{
-  const form=document.getElementById("serviceForm");
-  const success=document.getElementById("submitSuccess");
-  form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    success.style.display="block";
-    form.reset();
-  });
-});
