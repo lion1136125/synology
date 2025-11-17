55b502df45cc732229975892025e483b2e030779
lion1136125 <lion1496427@gmail.com>
2025-11-18 01:00:16 +0900

    Add files via upload

--- script-v2.js
+++ script-v2.js
@@ -1,1 +1,9 @@
-console.log('Synology script');
+document.addEventListener("DOMContentLoaded",()=>{
+  const form=document.getElementById("serviceForm");
+  const success=document.getElementById("submitSuccess");
+  form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    success.style.display="block";
+    form.reset();
+  });
+});
